adb825d91f3feefd4837f3d8cb549aeb46462f30
andresridi97 <57659709+andresridi97@users.noreply.github.com>
2019-11-14 03:57:23 -0600

    commit

--- app/src/main/java/com/example/horoscopo/Actvidad2.java
+++ app/src/main/java/com/example/horoscopo/Actvidad2.java
@@ -17,10 +17,6 @@ public class Actvidad2 extends AppCompatActivity {
         setContentView(R.layout.actvidad2);
         tvh = (TextView)findViewById(R.id.textView5);
         tve = (TextView)findViewById(R.id.textView3);
-        String dh = getIntent().getStringExtra("dias");
-        String mh = getIntent().getStringExtra("meses");
-        String ah = getIntent().getStringExtra("años");
-
     }
 
     //metodo para regresar
@@ -35,53 +31,55 @@ public class Actvidad2 extends AppCompatActivity {
         String result = String.valueOf(nan);
         tve.setText(result);
 
+        if(num_a == 1913 || num_a == 1925 || num_a == 1937 || num_a == 1949 || num_a == 1961 || num_a == 1973 || num_a == 1985 || num_a == 1997 || num_a == 2009) {
+            tvh.setText("rata");
+        }
+        if(num_a == 1914 || num_a == 1926 || num_a == 1938 || num_a == 1950 || num_a == 1962 || num_a == 1974 || num_a == 1986 || num_a == 1998 || num_a == 2010){
+            tvh.setText("búfalo");
+        }
 
-                if(num_d == 1913 || num_d == 1925 || num_d == 1937 || num_d == 1949 || num_d == 1961 || num_d == 1973 || num_d == 1985 || num_d == 1997 || num_d == 2009) {
-                    tvh.setText("rata");
-                }
-                if(num_d == 1914 || num_d == 1926 || num_d == 1938 || num_d == 1950 || num_d == 1962 || num_d == 1974 || num_d == 1986 || num_d == 1998 || num_d == 2010){
-                    tvh.setText("búfalo");
-                }
+        if(num_a == 1915 || num_a == 1927 || num_a == 1939 || num_a == 1951 || num_a == 1963 || num_a == 1975 || num_a == 1987 || num_a == 1999 || num_a == 2011){
+            tvh.setText("Tigre");
+        }
 
-                if(num_d == 1915 || num_d == 1927 || num_d == 1939 || num_d == 1951 || num_d == 1963 || num_d == 1975 || num_d == 1987 || num_d == 1999 || num_d == 2011){
-                    tvh.setText("Tigre");
-                }
+        if(num_a == 1916 || num_a == 1928 || num_a == 1940 || num_a == 1952 || num_a == 1964 || num_a == 1976 || num_a == 1988 || num_a == 2000 || num_a == 2012){
+            tvh.setText("conejo");
+        }
 
-                if(num_d == 1916 || num_d == 1928 || num_d == 1940 || num_d == 1952 || num_d == 1964 || num_d == 1976 || num_d == 1988 || num_d == 2000 || num_d == 2012){
-                    tvh.setText("conejo");
-                }
+        if(num_a == 1917 || num_a == 1929 || num_a == 1941 || num_a == 1953 || num_a == 1965 || num_a == 1977 || num_a == 1989 || num_a == 2001 || num_a == 2013){
+            tvh.setText("dragón");
+        }
 
-                if(num_d == 1917 || num_d == 1929 || num_d == 1941 || num_d == 1953 || num_d == 1965 || num_d == 1977 || num_d == 1989 || num_d == 2001 || num_d == 2013){
-                    tvh.setText("dragón");
-                }
+        if(num_a == 1918 || num_a == 1930 || num_a == 1942 || num_a == 1954 || num_a == 1966 || num_a == 1978 || num_a == 1990 || num_a == 2002 || num_a == 2014){
+            tvh.setText("serpiente");
+        }
 
-                if(num_d == 1918 || num_d == 1930 || num_d == 1942 || num_d == 1954 || num_d == 1966 || num_d == 1978 || num_d == 1990 || num_d == 2002 || num_d == 2014){
-                    tvh.setText("serpiente");
-                }
+        if(num_a == 1919 || num_a == 1931 || num_a == 1943 || num_a == 1955 || num_a == 1967 || num_a == 1979 || num_a == 1991 || num_a == 2003 || num_a == 2015){
+            tvh.setText("caballo");
+        }
 
-                if(num_d == 1919 || num_d == 1931 || num_d == 1943 || num_d == 1955 || num_d == 1967 || num_d == 1979 || num_d == 1991 || num_d == 2003 || num_d == 2015){
-                    tvh.setText("caballo");
-                }
+        if(num_a == 1920 || num_a == 1932 || num_a == 1944 || num_a == 1956 || num_a == 1968 || num_a == 1980 || num_a == 1992 || num_a == 2004 || num_a == 2016){
+            tvh.setText("cabra");
+        }
 
-                if(num_d == 1920 || num_d == 1932 || num_d == 1944 || num_d == 1956 || num_d == 1968 || num_d == 1980 || num_d == 1992 || num_d == 2004 || num_d == 2016){
-                    tvh.setText("cabra");
-                }
+        if(num_a == 1921 || num_a == 1933 || num_a == 1945 || num_a == 1957 || num_a == 1969 || num_a == 1981 || num_a == 1993 || num_a == 2005 || num_a == 2017){
+            tvh.setText("mono");
+        }
 
-                if(num_d == 1921 || num_d == 1933 || num_d == 1945 || num_d == 1957 || num_d == 1969 || num_d == 1981 || num_d == 1993 || num_d == 2005 || num_d == 2017){
-                    tvh.setText("mono");
-                }
+        if(num_a == 1922 || num_a == 1934 || num_a == 1946 || num_a == 1958 || num_a == 1970 || num_a == 1982 || num_a == 1994 || num_a == 2006 || num_a == 2018){
+            tvh.setText("gallo");
+        }
 
-                if(num_d == 1922 || num_d == 1934 || num_d == 1946 || num_d == 1958 || num_d == 1970 || num_d == 1982 || num_d == 1994 || num_d == 2006 || num_d == 2018){
-                    tvh.setText("gallo");
-                }
+        if(num_a == 1923 || num_a == 1935 || num_a == 1947 || num_a == 1959 || num_a == 1971 || num_a == 1983 || num_a == 1995 || num_a == 2007 || num_a == 2019){
+            tvh.setText("perro");
+        }
 
-                if(num_d == 1923 || num_d == 1935 || num_d == 1947 || num_d == 1959 || num_d == 1971 || num_d == 1983 || num_d == 1995 || num_d == 2007 || num_d == 2019){
-                    tvh.setText("perro");
-                }
+        if(num_a == 1924 || num_a == 1936 || num_a == 1948 || num_a == 1960 || num_a == 1972 || num_a == 1984 || num_a == 1996 || num_a == 2008 || num_a == 2020){
+            tvh.setText("cerdo");
+        }
 
-                if(num_d == 1924 || num_d == 1936 || num_d == 1948 || num_d == 1960 || num_d == 1972 || num_d == 1984 || num_d == 1996 || num_d == 2008 || num_d == 2020){
-                    tvh.setText("cerdo");
-                }
+
+
 
 
 
